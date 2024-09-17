9e4ae23aad0c26df244910edc4b65a9be3a060ca
rcbarreto <69176511+rcbarreto@users.noreply.github.com>
2024-09-17 22:46:02 +0000

    segunda vuelta

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["El vecino", "Luis Suarez", "El presidente", "Goku"];
+  let action = ["Miro", "Se despidio", "Aumento", "Rompio"];
+  let what = ["Mi ventana", "la selección", "El IRPF", "El Mundo"];
+  let when = [
+    "Apenas la abri",
+    "Mientras estaba en clase",
+    "Antes que cobre el sueldo",
+    "Cuando tiro una hengidama"
+  ];
+
+  function getRandomInt(cantidadElementos) {
+    let randomNumber = Math.floor(Math.random() * cantidadElementos);
+
+    return randomNumber;
+  }
+
+  let excusa = `
+  ${who[getRandomInt(who.length)]}
+  ${action[getRandomInt(action.length)]}
+  ${what[getRandomInt(what.length)]}
+  ${when[getRandomInt(when.length)]}
+`;
+
+  document.getElementById("excusas").innerHTML = excusa;
 };
